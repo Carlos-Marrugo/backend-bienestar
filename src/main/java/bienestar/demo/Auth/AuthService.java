2501d3203cd0d1688cd48b72bb56e48436cd4aa0
Carlos-Marrugo <elrprogramadortutoriales@gmail.com>
2024-10-11 17:46:53 -0500

    bugs to solve

--- src/main/java/bienestar/demo/Auth/AuthService.java
+++ src/main/java/bienestar/demo/Auth/AuthService.java
@@ -1,19 +1,34 @@
 package bienestar.demo.Auth;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
+import bienestar.demo.User.Role;
+import bienestar.demo.User.UserRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+
     public AuthResponse login(LoginRequest request) {
         return null;
     }
 
     public AuthResponse register(RegisterRequest request) {
-        return null;
+        User user = User.builder()
+            .username(request.getUsername())
+            .lastname(request.getLastname())
+            .firstname(request.getFirstname())
+            .country(request.getCountry())
+            .password(request.getPassword())
+            .role(Role.USER)
+            .build();
+
+            userRepository.save(user);
+
     }
 
 }
---
+++ src/main/java/bienestar/demo/User/UserRepository.java
@@ -0,0 +1,9 @@
+package bienestar.demo.User;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+    Optional<User> findByUsername(String username);
+}
