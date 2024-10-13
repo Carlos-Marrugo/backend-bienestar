e9dae49bc030135840f08d08931f8c5a58ba1aef
Carlos <elrprogramadortutoriales@gmail.com>
2024-10-13 16:43:13 -0500

    Configure JWT security, fix authentication provider and improve filter setup

--- src/main/java/bienestar/demo/Config/SecurityConfig.java
+++ src/main/java/bienestar/demo/Config/SecurityConfig.java
@@ -2,28 +2,38 @@ package bienestar.demo.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import lombok.RequiredArgsConstructor;
+import bienestar.demo.Jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider; // Inyección de AuthenticationProvider
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf
-                        .disable())
+                .csrf(csrf -> csrf.disable())  
                 .authorizeHttpRequests(authRequest -> authRequest
-                        .requestMatchers("/auth/**").permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(withDefaults())
-                .build();
-
+                        .requestMatchers("/auth/**").permitAll() 
+                        .anyRequest().authenticated()  
+                )
+                .sessionManagement(sessionManager -> 
+                        sessionManager
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  
+                )
+                .authenticationProvider(authenticationProvider) 
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) 
+                .build(); 
     }
 }
